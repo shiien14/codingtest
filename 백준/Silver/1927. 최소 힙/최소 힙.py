d48becc22fe2589a6b9f9bfd125710b8db8ff616
shiien14 <98999139+shiien14@users.noreply.github.com>
2025-04-14 09:38:40 +0900

    [Silver II] Title: 최소 힙, Time: 108 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+N = int(input())
+heap=[]
+
+for _ in range(N):
+    num = int(input())
+    if num==0:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, num)
