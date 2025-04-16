1abb98c9febe9867efac1c4ae462c858aa7bf18a
shiien14 <98999139+shiien14@users.noreply.github.com>
2025-04-16 09:42:04 +0900

    [Silver V] Title: 다리 놓기, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,11 @@
+import sys
+import math
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    n, m = map(int, input().split())
+    result = math.factorial(m) // (math.factorial(n) * math.factorial(m - n))
+    print(result)
