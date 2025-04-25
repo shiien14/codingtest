062fd65431a02069fb10589f405863660449d40a
shiien14 <98999139+shiien14@users.noreply.github.com>
2025-04-25 09:43:44 +0900

    [Silver III] Title: 바이러스, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,30 +1,25 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10000)
 input=sys.stdin.readline
 
-N=int(input()) # 컴퓨터 수
-L=int(input()) # 연결 라인 수
-line=[[] for _ in range(N+1) ]
+N=int(input())
+L=int(input())
+line=[[] for _ in range(N+1)]
 
 for _ in range(L):
     a,b=map(int, input().split())
     line[a].append(b)
     line[b].append(a)
 
-#print(line)
+visited=[0]*(N+1)
 
-cnt=0
-visited=[0]*(N+1) #방문확인
-visited[0]=1
-visited[1]=1
-
-def dfs(R, cnt):
+def dfs(R):
     visited[R]=1  
-    tmp = 1
+    count = 1
     for i in line[R]:
         if not visited[i]:
-            tmp += dfs(i, cnt + tmp)
+            count += dfs(i)
             
-    return tmp
+    return count
 
-print(dfs(1, 0)-1)
+print(dfs(1)-1)
