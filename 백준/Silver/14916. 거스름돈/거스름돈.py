7e75af95f58101fd3bec5aca56894551474ee0eb
shiien14 <98999139+shiien14@users.noreply.github.com>
2025-05-27 10:22:37 +0900

    [Silver V] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+cnt=0
+
+while n>0:
+    if n % 5==0:
+        cnt += n//5
+        break
+    n-=2
+    cnt+=1
+if n<0:
+    print(-1)
+else:
+    print(cnt)
