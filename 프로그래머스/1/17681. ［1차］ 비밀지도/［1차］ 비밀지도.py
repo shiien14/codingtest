82a1fd1e81e1878ff1b3700c7c968e3cb5bc5ce4
shiien14 <98999139+shiien14@users.noreply.github.com>
2025-07-25 09:27:41 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,24 +1,9 @@
 def solution(n, arr1, arr2):
-    result= []
-    map1 = []
-    map2 = []
-    for num1 in arr1:
-        num=bin(num1)[2:]
-        if len(num) <n:
-            num="0"*(n-len(num))+num
-        map1.append(num)
-    for num2 in arr2:
-        num=bin(num2)[2:]
-        if len(num) <n:
-            num="0"*(n-len(num))+num
-        map2.append(num)
-
-    for i in range(n):
-        answer=""
-        for j in range(n):
-            if map1[i][j] == "1" or map2[i][j] == "1":
-                answer+="#"
-            else:
-                answer+=" "
-        result.append(answer)
-    return result
+    answer = []
+    for i,j in zip(arr1,arr2):
+        a12 = str(bin(i|j)[2:])
+        a12=a12.rjust(n,'0')
+        a12=a12.replace('1','#')
+        a12=a12.replace('0',' ')
+        answer.append(a12)
+    return answer
