e28f4be1a61d56e257e3f7d31fd83519dfce1a5b
shiien14 <98999139+shiien14@users.noreply.github.com>
2025-07-25 09:43:59 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3634.63 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,7 +1,7 @@
 def solution(elements):
-    answer = []
+    answer = set()
     elements2=elements*2
     for n in range(1,len(elements)+1):
-        for i in range(0,len(elements2)):
-            answer.append(sum(elements2[i:i+n]))
-    return len(set(answer))
+        for i in range(len(elements)):
+            answer.add(sum(elements2[i:i+n]))
+    return len(answer)
