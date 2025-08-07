910eb149eaa31be4be87dab02f02b3b7205b2373
shiien14 <98999139+shiien14@users.noreply.github.com>
2025-08-07 09:56:18 +0900

    [level 2] Title: 롤케이크 자르기, Time: 685.97 ms, Memory: 49.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def solution(topping):
+    answer = 0
+    left = set()
+    right = Counter(topping)
+
+    for t in topping:
+        left.add(t)
+        right[t] -= 1
+        if right[t] == 0:
+            del right[t]
+        if len(left) == len(right):
+            answer += 1
+
+    return answer
