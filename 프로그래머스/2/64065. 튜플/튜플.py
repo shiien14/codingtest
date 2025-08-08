53e557ae300e3472c79377c5aa39b894c102cd09
shiien14 <98999139+shiien14@users.noreply.github.com>
2025-08-08 10:02:56 +0900

    [level 2] Title: 튜플, Time: 342.91 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,13 +1,8 @@
 def solution(s):
     answer=[]
-    par=list(s.split("{"))
-    result = []
-    
-    for p in par:
-        if p != "":
-            result.append(p.split("}")[0])   
+    result = s.lstrip('{').rstrip('}').split('},{')
 
-    result.sort(key = lambda x : len(x))
+    result.sort(key = len)
     
     for r in result:
         for a in map(int,r.split(',')):
