55efa26edb0abdcb70c7fd518dc43506d195a3c1
shiien <98999139+shiien14@users.noreply.github.com>
2025-09-11 09:34:03 +0900

    [Silver V] Title: 올림픽, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -7,20 +7,23 @@ n, k = map(int, input().split())
 countries = []
 for i in range(n):
     country, gold, silver, bronze = map(int, input().split())
-    sum=gold*3+silver*2+bronze
-    countries.append([sum, country])
+    countries.append([gold, silver, bronze, country])
 
 countries.sort(reverse=True)
 
 countries[0].append(1)
-cnt=1
+
+if countries[0][3] == k:
+    print(1)
+    sys.exit(0)
+
 for i in range(1, n):
-    cnt+=1
-    if countries[i][0] == countries[i-1][0]:
+    rank_num = i + 1
+    if countries[i][0] == countries[i-1][0] and countries[i][1] == countries[i-1][1] and countries[i][2] == countries[i-1][2]:
         countries[i].append(countries[i-1][-1])
     else:
-        countries[i].append(cnt)
-    if countries[i][1] == k:
+        countries[i].append(rank_num)
+    if countries[i][3] == k:
         print(countries[i][-1])
         break
     
