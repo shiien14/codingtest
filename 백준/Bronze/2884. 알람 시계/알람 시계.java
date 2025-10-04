540370494fa13ecfb990e5614cf1cf8738458471
shiien <98999139+shiien14@users.noreply.github.com>
2025-10-04 16:55:23 +0900

    [Bronze III] Title: 알람 시계, Time: 208 ms, Memory: 18560 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+
+        M-=45;
+        
+        if (M<0){
+            M+=60;
+            H-=1;
+        }
+        if (H<0){
+            H+=24;
+        }
+        
+        System.out.println(H+" "+M);
+    }
+}
