7a7651481cb95f855024472cd5b40b3ec586bf91
shiien <98999139+shiien14@users.noreply.github.com>
2025-10-22 18:39:47 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,14 @@
+import sys
+
+input = sys.stdin.readline
+scores=[]
+cnt=0
+grades={'A+':4.5,'A0':4.0,'B+':3.5,'B0':3.0,'C+':2.5,'C0':2.0,'D+':1.5,'D0':1.0,'F':0.0}
+
+for _ in range(20):
+    name, score, grade = input().rstrip().split()
+    if grade != 'P':
+        scores.append(float(score)*grades[grade])
+        cnt+=float(score)
+
+print(sum(scores)/cnt)
