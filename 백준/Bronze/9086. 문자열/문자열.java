f24352c4c35e72f1ccf5e4af83a9759b7c662369
shiien <98999139+shiien14@users.noreply.github.com>
2025-10-30 15:48:05 +0900

    [Bronze V] Title: 문자열, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i=0;i<T;i++){
+            String s = br.readLine();
+            System.out.print(s.charAt(0));
+            System.out.println(s.charAt(s.length()-1));
+        }
+    }
+}
