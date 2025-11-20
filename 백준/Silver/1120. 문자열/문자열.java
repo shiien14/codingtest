854af3f997417d054061eecc3c73c94e939e2593
shiien <98999139+shiien14@users.noreply.github.com>
2025-11-20 09:22:45 +0900

    [Silver IV] Title: 문자열, Time: 104 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String A = st.nextToken();
+        String B = st.nextToken();
+        int result = A.length();
+        
+        for(int i=0; i<=B.length()-A.length(); i++){
+            int count = 0;
+            for(int j=0; j<A.length(); j++){
+                if(A.charAt(j)!=B.charAt(j+i)){
+                    count ++;
+                }
+            }
+            result = Math.min(count, result);
+        }
+        System.out.println(result);
+    }
+}
